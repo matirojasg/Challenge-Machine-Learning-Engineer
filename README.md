0fe95c54168427fce406ace50f89337b429e91f1
Matías Rojas <matirojasga@gmail.com>
2023-04-09 22:53:54 -0400

    Solución v1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Challenge-Machine-Learning-Engineer
-Desafío para el puesto ML Engineer en Latam Airlines
+Desafío para el puesto ML Engineer en Latam Airlines.
+
+La respuesta a las preguntas se encuentra en el notebook `solution.ipynb`.
+
+Cualquier consulta a matirojasga@gmail.com
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-# Definir los datos de entrada
+# Datos del vuelo a predecir.
 input_data = {'OPERA': 'Aeromexico', 'SIGLADES': 'MIAMI', 'MES': 7, 'DIANOM': 'Viernes', 'temporada_alta': 1, 'periodo_dia': 'mañana', 'hora': 8, 'vuelos_paralelos': 5}
 
-# Hacer una solicitud POST a la ruta '/predict'
-url = 'http://0.0.0.0:8000/predict'
+# Hacer una solicitud POST a la ruta '/predict'.
+url = 'http://localhost:8000/predict'
 headers = {'Content-type': 'application/json'}
 response = requests.post(url, data=json.dumps(input_data), headers=headers)
 
