924ff11eb561c5d7ee5b62a8eb36b21bdd678077
Matías Rojas <matirojasga@gmail.com>
2023-04-09 23:33:41 -0400

    SoluciÃón v2.0

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ import uvicorn
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-model = pickle.load(open('models/model.pkl', 'rb'))
-original_columns = pickle.load(open('models/columns.pkl', 'rb'))
 
 # Definir las columnas categóricas y numéricas
-cat_cols = ['OPERA', 'SIGLADES', 'DIANOM', 'periodo_dia']
-num_cols = ['MES', 'temporada_alta', 'hora', 'vuelos_paralelos']
+cat_cols = ["OPERA", "SIGLADES", "DIANOM", "periodo_dia"]
+num_cols = ["MES", "temporada_alta", "hora", "vuelos_paralelos"]
 
 # Definir la clase que representa el modelo de entrada
 class InputModel(BaseModel):
@@ -23,11 +21,14 @@ class InputModel(BaseModel):
     DIANOM: str
     periodo_dia: str
 
+
 # Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
+model = pickle.load(open("models/model.pkl", "rb"))
+original_columns = pickle.load(open("models/columns.pkl", "rb"))
 # Ruta de la API REST para hacer predicciones
-@app.post('/predict')
+@app.post("/predict")
 def predict(input_data: InputModel):
     # Crear un dataframe a partir de los datos de entrada
     input_df = pd.DataFrame([input_data.dict()])
@@ -35,22 +36,18 @@ def predict(input_data: InputModel):
     # Aplicar el preprocesamiento a los datos de entrada
     X = pd.get_dummies(input_df, columns=cat_cols)
 
-    scaler = StandardScaler() 
+    scaler = StandardScaler()
     X[num_cols] = scaler.fit_transform(X[num_cols])
 
     # Realizar la predicción utilizando el modelo
-    X = X.reindex(labels = original_columns, axis = 1, fill_value = 0)
+    X = X.reindex(labels=original_columns, axis=1, fill_value=0)
     prediction = model.predict(X)[0]
     proba = model.predict_proba(X).tolist()
-    proba = list(map(lambda x: round(x,2), proba[0]))
+    proba = list(map(lambda x: round(x, 2), proba[0]))
 
     # Devolver la predicción en formato JSON
-    return {
-        'prediction': int(prediction),
-        'proba': proba
-    }
+    return {"prediction": int(prediction), "proba": proba}
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
-
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import requests
 import json
 
 # Datos del vuelo a predecir.
-input_data = {'OPERA': 'Aeromexico', 'SIGLADES': 'MIAMI', 'MES': 7, 'DIANOM': 'Viernes', 'temporada_alta': 1, 'periodo_dia': 'mañana', 'hora': 8, 'vuelos_paralelos': 5}
+input_data = {
+    "OPERA": "Aeromexico",
+    "SIGLADES": "MIAMI",
+    "MES": 7,
+    "DIANOM": "Viernes",
+    "temporada_alta": 1,
+    "periodo_dia": "mañana",
+    "hora": 8,
+    "vuelos_paralelos": 5,
+}
 
 # Hacer una solicitud POST a la ruta '/predict'.
-url = 'http://localhost:8000/predict'
-headers = {'Content-type': 'application/json'}
+url = "http://localhost:8000/predict"
+headers = {"Content-type": "application/json"}
 response = requests.post(url, data=json.dumps(input_data), headers=headers)
 
 # Imprimir la respuesta
